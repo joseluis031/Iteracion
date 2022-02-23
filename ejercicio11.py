35e1d767da4743dc2c2affaf49b7b004f2a3d247
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-23 16:34:32 +0000

    ejercicio 12 terminado

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,11 +1,28 @@
-#definimos la variable del máximo común divisor de euclides.
-elegir = int(input("Elige: Euclides o sumas y rectas "))
-x= input("Introduce el valor del divisor: ")
-y= input("Introduce el valor del divisor: ")
-mcd = x/y
-if mcd == 0:
-  print("El máximo común divisor es " , y , ".")
-if mcd != 0:
-  resto= x%y
-  mcd_euclidesnuevo = y / resto
-      
+""#definimos la variable del máximo común divisor de euclides.
+elegir = input("Elige: Euclides o sumas y rectas ")
+x= int(input("Introduce el valor del divisor: "))
+y= int(input("Introduce el valor del divisor: "))
+if elegir == "euclides":
+  def mcd(x, y):
+    if mcd == 0:
+     print("El máximo común divisor es " , y , ".")
+    if mcd != 0:
+     resto= x%y
+    mcd_euclidesnuevo = y / resto
+
+elif elegir == "sumas y rectas":
+   def mcd(x, y):
+    if x ==0 :
+      return y
+    elif y == 0:
+      return x
+    elif x == y:
+      return x
+    elif x > y:
+      return mcd(x, y-x)
+    return mcd(x, y-x)
+    else:
+      print("")
+  
+
+     
--- ejercicio12.py
+++ ejercicio12.py
@@ -1,4 +1,18 @@
 #importamos librerias
 import math
 
-a = int (input("Para obtener todos los cuadrados perfectos entre o y un número, introduce cuadrado, pero si dea realizar la raíz cuadrada de un número entero, introduce raiz: ))
+a = input("Para obtener todos los cuadrados perfectos entre o y un número, introduce cuadrado, pero si dea realizar la raíz cuadrada de un número entero, introduce raiz: ")
+
+if a == "cuadrado":
+  def cuadrado1 (x,y):
+    if (x**2) <= b:
+      print(x**2)
+      cuadrado1(0,int(input("Introduce el valos hasta el que deseas que llegue su lista de cuadrados: ")))
+
+if a == "raiz":
+  b=int(input("Introduce un número entero para el cual desee obtener su raíz cuadrada: "))
+  raiz1 = b **(1/2)
+  print("La raiz cuadrada de " , b , " es " , raiz1)
+
+if a != "cuadrado" and a!= "raiz":
+      print("Introduce un valor correcto.")
