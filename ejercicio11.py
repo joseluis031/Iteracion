a1459dd0ea3aedd7324041f72e27943709953843
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-23 16:22:29 +0000

    Creo variable para seleccionar el cuadrado o la raiz

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,5 +1,5 @@
 #definimos la variable del máximo común divisor de euclides.
-
+elegir = int(input("Elige: Euclides o sumas y rectas "))
 x= input("Introduce el valor del divisor: ")
 y= input("Introduce el valor del divisor: ")
 mcd = x/y
---
+++ ejercicio12.py
@@ -0,0 +1,4 @@
+#importamos librerias
+import math
+
+a = int (input("Para obtener todos los cuadrados perfectos entre o y un número, introduce cuadrado, pero si dea realizar la raíz cuadrada de un número entero, introduce raiz: ))
