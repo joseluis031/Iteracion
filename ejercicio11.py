42de5028b313e96814f10f7ab0c2cadb1e9337c1
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-23 11:59:59 +0000

    commit 1

--- ejercicio11.py
+++ ejercicio11.py
@@ -3,3 +3,11 @@
 x= input("Introduce el valor del divisor: ")
 y= input("Introduce el valor del divisor: ")
 mcd_euclides = x/y
+if mcd_euclides == 0:
+  print("El máximo común divisor es " , y , ".")
+if mcd_euclides != 0:
+  resto= x % y
+  mcd_euclidesnuevo= y / resto
+  while mcd_euclidesnuevo !=0:
+    
+  
