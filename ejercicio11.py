945cefe7123faff78f54671e5818942f9a03b5a9
Flechaa7 <91721888+joseluis031@users.noreply.github.com>
2022-02-23 17:45:41 +0000

    ejercicio 11 acabado

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,18 +1,17 @@
-""#definimos la variable del máximo común divisor de euclides.
 elegir = input("Elige: euclides o sumas y rectas ")
 x= int(input("Introduce el valor del divisor: "))
 y= int(input("Introduce el valor del divisor: "))
- 
+
 if elegir == "euclides":
   def mcd(x, y):
-    if mcd == 0:
-     print("El máximo común divisor es " , y , ".")
-    if mcd != 0:
-      resto= x%y
-      mcd_euclides = y / resto
+    if y == 0:
+     return x
+    return mcd(y, x%y)
+  final = mcd(x, y)
+  print(final)
 
 elif elegir == "sumas y rectas":
-   def mcd(x, y):
+  def mcd(x, y):
     if x ==0 :
       return y
     elif y == 0:
@@ -20,9 +19,13 @@ elif elegir == "sumas y rectas":
     elif x == y:
       return x
     elif x > y:
-      return mcd(x, y-x)
-    else:
-      print("No existe m.c.d") 
+      return mcd(x-y, y)
+    return mcd(x, y-x)
+  if(mcd(x, y)):
+    final = mcd(x, y)
+    print(final)
+else:
+  print("No existe m.c.d")
   
 
      
