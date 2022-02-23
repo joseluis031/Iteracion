48506cf5354f35071c412c0b09b0731f00f5a5c3
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-02-23 12:10:22 +0000

    establezco la media de los importes de los movimientos

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,3 +1,4 @@
+operaciones = []
 cuenta = input("Desea abrir una cuenta en el banco (si/no): ")
 
 if cuenta == "no":
@@ -6,23 +7,28 @@ if cuenta == "si":
     saldo = 0
     print("Primero debe abonar dinero en la cuenta.")
     abono= int(input("Introduzca el dinera desea abonar:"))
-    saldo = abono
+    saldo = (abono).add(operaciones)
 
 operacion = input("¿Desea hacer otra operación más como consultar dinero,retirar o abonar?")
 if operacion == "no":
     print("Vale gracias por consultar este banco. Hasta pronto.")
 if operacion == "abonar":
     introducir_dinero= float(input("Introduzca el dinero de desea ingresar en la cuenta: "))
-    saldo = saldo + introducir_dinero
+    saldo = (saldo + introducir_dinero).add(operaciones)
     print("Su saldo es de " , saldo , "€")
 if operacion == "consultar":
     print("El saldo de su cuenta es " , saldo , "€")
 if operacion == "retirar":
     retirar_dinero= float(input("Introduzca el dinero que desea retirar: "))
-    saldo = saldo - retirar_dinero
+    saldo = (saldo - retirar_dinero).add(operaciones)
     if saldo >= 0:
         print("Con la retirada de " , retirar_dinero , " su saldo disminuye a un total de " , saldo , "€")
     if saldo < 0:
         saldo = saldo * -1
         print("Con la retirada de " , retirar_dinero ,"Usted no tiene saldo en la cuenta, además debe un total de  " , saldo , "€")
-    
+
+suma= 20
+if sum(operaciones) > suma: 
+  print("La media de los importes de los movimientos es superior al mínimo establecido")
+else:
+  print("La media de los importes de los movimientos es inferior al mínimo establecido")
